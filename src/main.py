99f9c9ea6beaff2eeabbfe9a181efb6bb3f1c728
DeutschPinguin <179315969+DeutschPinguin@users.noreply.github.com>
2026-03-05 15:09:22 +0300

    d

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from tkinter import Tk, ttk
-import menu, hotkeys
+import tkinter, files, menu, hotkeys, texteditor
 
 
 def main ():
     root = Tk()
     root.title('==- ULTIMATE TEXT EDITOR 2000 -==')
     
+    fr = ttk.Frame(root, padding=(3, 3, 12, 12))
+    fr.grid(column=0, row=0, sticky='nwes')
+
+    files.init(root)
     menu.init(root)
     hotkeys.init(root)
+    texteditor.init(root)
 
     root.mainloop()
 
--- src/test.py
+++
@@ -1,7 +0,0 @@
-from tkinter import Tk, ttk
-
-
-def addTo (root: Tk):
-    frame = ttk.Frame(root, padding=10)
-    frame.grid()
-    ttk.Button(frame, text='Quit', command=root.destroy).grid(column=1, row=0)
